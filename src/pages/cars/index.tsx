1257e7642a304d97a84c976f29a09707daa90bf2
xta <luoihocbk@gmail.com>
2025-05-28 12:00:24 +0200

    clean code

--- src/pages/cars/index.tsx
+++ src/pages/cars/index.tsx
@@ -27,33 +27,3 @@ const CarsPage: React.FC = () => {
 };
 
 export default CarsPage;
-
-
-
-// import Link from 'next/link'
-// import styles from '../../../styles/Home.module.css'
-//
-// export default function CarsList(props) {
-//   return (
-//     <div className={styles.container}>
-//       <main className={styles.main}>
-//         <h1 className={styles.title}>
-//           Cars list
-//         </h1>
-//
-//         <ul>
-//           <li>
-//             <Link href="/cars/tesla">Tesla</Link>
-//           </li>
-//           <li>
-//             <Link href="/cars/ford">Ford</Link>
-//           </li>
-//           <li>
-//             <Link href="/cars/lambo">Lambo</Link>
-//           </li>
-//         </ul>
-//         </main>
-//     </div>
-//   )
-//   }
-//
